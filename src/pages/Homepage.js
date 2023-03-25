65fe71646a13ccd30fa1b6166353c3df8aec14e7
shellyalmo <shellyalmo@gmail.com>
2023-03-25 16:24:32 +0300

    show users  list on homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,33 +1,9 @@
-import { useState, useEffect } from "react";
-import { bankApi } from "../api/api";
-import Navbar from "../components/Navbar";
+import UsersList from "./UsersList";
 
 const Homepage = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    async function fetchUsers() {
-      try {
-        const response = await bankApi.get("/users");
-        setUsers(response.data.data);
-        console.log("Users:", users);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    fetchUsers();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <>
-      <Navbar />
-      <div>Welcome to Homepage</div>
-      <div>
-        {users?.map((user) => {
-          return user.name;
-        })}
-      </div>
+      <UsersList />
     </>
   );
 };
---
+++ src/pages/UsersList.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import { bankApi } from "../api/api";
+import Navbar from "../components/Navbar";
+
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const response = await bankApi.get("/users");
+        setUsers(response.data.data);
+        console.log("Users:", users);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    fetchUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+
+      <div>
+        {users?.map((user) => {
+          return user.name;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default UsersList;
